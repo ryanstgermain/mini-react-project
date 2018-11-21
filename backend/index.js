5358bec8bafb8bfe368833bf75e16417c7ac1dfe
ryanstgermain <ryanjoel26@gmail.com>
2018-11-21 12:35:26 -0700

    populated data for data.json and have all my requests and error handlers inside index.js

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,50 @@
-var express = require('express')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
 var cors = require('cors')
-var app = express()
+var data = require('./data.json')
+
+app.use(bodyParser.json())
 
 app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send({ data })
+})
+
+app.get('/:id', (req, res) => {
+    var { id } = req.params
+    var filtered = data.filter(obj => {
+        return obj.id == id
+    })
+    res.send({ data: filtered })
+})
+
+app.post('/', (req, res) => {
+    var { body } = req
+    var obj = {
+        id: data.length + 1,
+        ability: body.name
+    }
+    data.push(obj)
+    res.send({ data: obj })
+})
+
+app.put('/:id', (req, res) => {
+    var { body } = req
+    var { id } = req.params
+
+    var mapped = data.map(obj => {
+        if(id == obj.id){
+            obj = {
+                id: obj.id,
+                ...body
+            }
+        } return obj
+    })
+    data = mapped
+    res.send({ data })
+})
 
 app.use(function (req, res, next) {
     res.status(404).send("Sorry I can't find that!")
@@ -13,8 +55,11 @@ app.use(function (err, req, res, next) {
     res.status(500).send('Something broke...')
 })
 
-app.get('/products/:id', function(req, res, next) {
-    res.json({msg: 'This is CORS-enabled for all origins!'})
+app.delete('/:id', (req, res) => {
+    var { id } = req.params
+    var mapped = data.filter(obj => {
+        return obj.id != id
+    })
 })
 
 app.listen(3001, function () {
